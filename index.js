824794c5705af1b5de4169420d1ce2d37ca2bbb4
Riyazabbas <mohamedriyazabbas@gmail.com>
2025-06-01 12:11:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,3 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
