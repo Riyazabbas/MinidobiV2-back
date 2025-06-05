21450c21c698ae5b087e2178659bdb127d847f52
Riyazabbas <mohamedriyazabbas@gmail.com>
2025-06-05 13:45:34 +0800

    newUpdate index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+
 const app = express();
 let ledStatus = "off";
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Needed for ToyyibPay callback
 
 // LED status
 app.get("/status", (req, res) => {
